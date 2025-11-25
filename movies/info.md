c7e9218f60e9485fb5882a529f16a6bc6e43d41e
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-11-24 19:29:40 -0500

    feat: Add IMDb movie list scraping script, initial OMDb API scraper, and generated movie data CSV.

---
+++ movies/info.md
@@ -0,0 +1,3 @@
+## Movie Info
+
+Scrape movie info from IMDB
---
+++ movies/scrape.py
@@ -0,0 +1,6 @@
+
+class scraper:
+
+    def __init__(self, api_key="use_local"):
+        
+        self.base_url = "http://www.omdbapi.com/"
